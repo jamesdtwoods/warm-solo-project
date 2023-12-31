e79ec6362d3b7357be1f8a9bfd0508213cd61a7b
James Woods <james.d.t.woods@gmail.com>
2023-12-31 14:07:57 -0600

    cleaned up clothing list, added clothing type headers and REAL conditional rendering

--- src/components/ClothingList/ClothingList.jsx
+++ src/components/ClothingList/ClothingList.jsx
@@ -2,10 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import ClothingItem from '../ClothingItem/ClothingItem';
 
-
 function ClothingList() {
   const dispatch = useDispatch();
-
   const clothesList = useSelector(store => store.clothingReducer.clothingList);
 
   useEffect(() => {
@@ -14,91 +12,82 @@ function ClothingList() {
         })
   }, []); 
 
-  const isHat = (item) => {
-    return item.clothing_type_id === 1;
+  const checkFunction = (clothesList, clothing_type_id) => {
+    let check = false;
+    for (let item of clothesList) {
+      if (item.clothing_type_id === clothing_type_id) {
+        check = true;
+      }
+    }
+    return check;
   }
-  const isGloves = (item) => {
-    return item.clothing_type_id === 2;
+  const mapFunction = (clothingItem) => {
+    return(
+      <>
+        <ul>
+          <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
+        </ul>
+      </>
+    )
   }
-  const isSocks = (item) => {
-    return item.clothing_type_id === 3;
-  }
-  const isBaseTorso = (item) => {
-    return item.clothing_type_id === 4;
-  }
-  const isBaseLegs = (item) => {
-    return item.clothing_type_id === 5;
-  }
-  const isJacket = (item) => {
-    return item.clothing_type_id === 6;
-  }
-  const isPants = (item) => {
-    return item.clothing_type_id === 7;
-  }
-  const isAccessories = (item) => {
-    return item.clothing_type_id === 8;
-  }
-  const isOther = (item) => {
-    return item.clothing_type_id === 9;
+  const filterFunction = (clothesList, clothing_type_id) => {
+    let itemArray=[]
+    for (let item of clothesList) {
+      if(item.clothing_type_id === clothing_type_id){
+        itemArray.push(item);
+      }
+    }
+    return itemArray
   }
 
   return (
     <div className="container">
       <h3>This is the clothing list</h3>
-        {clothesList.filter(isHat).map((clothingItem) => (
-          <>
-            <h3>Hats:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>
-        ))}
-        {clothesList.filter(isGloves).map((clothingItem) => (
-          <>
-            <h3>Gloves:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>  
-        ))}
-        {clothesList.filter(isSocks).map((clothingItem) => (
-          <>
-            <h3>Socks:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>
-        ))}
-        {clothesList.filter(isBaseTorso).map((clothingItem) => (
-          <>
-            <h3>Base Layer - Torso:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>
-        ))}
-        {clothesList.filter(isBaseLegs).map((clothingItem) => (
-          <>
-            <h3>Base Layer - Legs:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>
-        ))}
-        {clothesList.filter(isJacket).map((clothingItem) => (
-          <>
-            <h3>Jackets:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>  
-        ))}
-        {clothesList.filter(isPants).map((clothingItem) => (
-          <>
-            <h3>Pants:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>  
-        ))}
-        {clothesList.filter(isAccessories).map((clothingItem) => (
-          <>
-            <h3>Accessories:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>  
-        ))}
-        {clothesList.filter(isOther).map((clothingItem) => (
-          <>
-            <h3>Other:</h3>
-            <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
-          </>  
-        ))}
+      {checkFunction(clothesList, 1) ? 
+        <> <h3>Hats:</h3> 
+          {filterFunction(clothesList, 1).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 2) ? 
+        <> <h3>Gloves:</h3> 
+          {filterFunction(clothesList, 2).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 3) ? 
+        <> <h3>Socks:</h3> 
+          {filterFunction(clothesList, 3).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 4) ? 
+        <> <h3>Base Layer - Torso:</h3> 
+          {filterFunction(clothesList, 4).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 5) ? 
+        <> <h3>Base Layer - Legs:</h3> 
+          {filterFunction(clothesList, 5).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 6) ? 
+        <> <h3>Jackets:</h3> 
+          {filterFunction(clothesList, 6).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 7) ? 
+        <> <h3>Pants:</h3> 
+          {filterFunction(clothesList, 7).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 8) ? 
+        <> <h3>Accessories:</h3> 
+          {filterFunction(clothesList, 8).map(mapFunction)}
+        </> 
+      : <></>}
+      {checkFunction(clothesList, 9) ? 
+        <> <h3>Other:</h3> 
+          {filterFunction(clothesList, 9).map(mapFunction)}
+        </> 
+      : <></>}
     </div>
   );
 }
