c7117109c023e7bf7d322e7339773b16a1df3657
James Woods <james.d.t.woods@gmail.com>
2023-12-31 14:30:40 -0600

    added add clothes button and more conditional rendering if no clothes exist

--- src/components/ClothingList/ClothingList.jsx
+++ src/components/ClothingList/ClothingList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import ClothingItem from '../ClothingItem/ClothingItem';
 
 function ClothingList() {
   const dispatch = useDispatch();
+  const history = useHistory()
   const clothesList = useSelector(store => store.clothingReducer.clothingList);
 
   useEffect(() => {
@@ -11,6 +13,14 @@ function ClothingList() {
           type: 'SAGA/FETCH_CLOTHES'
         })
   }, []); 
+
+  const checkAllFunction = (clothesList) => {
+    let check = false;
+    for (let item of clothesList) {
+      check = true;
+    }
+    return check;
+  }
 
   const checkFunction = (clothesList, clothing_type_id) => {
     let check = false;
@@ -39,54 +49,66 @@ function ClothingList() {
     })
   }
 
+  const addClothes = () => {
+    history.push(`/newClothes`)
+  }
+
   return (
     <div className="container">
-      <h3>This is the clothing list</h3>
-      {checkFunction(clothesList, 1) ? 
-        <> <h3>Hats:</h3> 
-          {mapFunction(clothesList, 1)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 2) ? 
-        <> <h3>Gloves:</h3> 
-          {mapFunction(clothesList, 2)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 3) ? 
-        <> <h3>Socks:</h3> 
-          {mapFunction(clothesList, 3)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 4) ? 
-        <> <h3>Base Layer - Torso:</h3> 
-          {mapFunction(clothesList, 4)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 5) ? 
-        <> <h3>Base Layer - Legs:</h3> 
-          {mapFunction(clothesList, 5)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 6) ? 
-        <> <h3>Jackets:</h3> 
-          {mapFunction(clothesList, 6)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 7) ? 
-        <> <h3>Pants:</h3> 
-          {mapFunction(clothesList, 7)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 8) ? 
-        <> <h3>Accessories:</h3> 
-          {mapFunction(clothesList, 8)}
-        </> 
-      : <></>}
-      {checkFunction(clothesList, 9) ? 
-        <> <h3>Other:</h3> 
-          {mapFunction(clothesList, 9)}
-        </> 
-      : <></>}
+      {checkAllFunction(clothesList) ?  
+        <div>
+          <button onClick={addClothes}>Add Clothes</button>
+          <h2>Clothing List</h2>
+          {checkFunction(clothesList, 1) ? 
+            <> <h3>Hats:</h3> 
+              {mapFunction(clothesList, 1)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 2) ? 
+            <> <h3>Gloves:</h3> 
+              {mapFunction(clothesList, 2)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 3) ? 
+            <> <h3>Socks:</h3> 
+              {mapFunction(clothesList, 3)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 4) ? 
+            <> <h3>Base Layer - Torso:</h3> 
+              {mapFunction(clothesList, 4)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 5) ? 
+            <> <h3>Base Layer - Legs:</h3> 
+              {mapFunction(clothesList, 5)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 6) ? 
+            <> <h3>Jackets:</h3> 
+              {mapFunction(clothesList, 6)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 7) ? 
+            <> <h3>Pants:</h3> 
+              {mapFunction(clothesList, 7)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 8) ? 
+            <> <h3>Accessories:</h3> 
+              {mapFunction(clothesList, 8)}
+            </> 
+          : <></>}
+          {checkFunction(clothesList, 9) ? 
+            <> <h3>Other:</h3> 
+              {mapFunction(clothesList, 9)}
+            </> 
+          : <></>}
+        </div>
+        : <>
+            <h1>No clothes in your inventory</h1>
+            <button onClick={addClothes}>Add Clothes</button>
+          </>}
     </div>
   );
 }
