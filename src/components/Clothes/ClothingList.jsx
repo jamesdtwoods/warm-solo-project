e63b365abfe05180befd0113f22baeaa008c9ee0
James Woods <james.d.t.woods@gmail.com>
2024-01-06 09:56:59 -0600

    cleaned up clothes

--- src/components/Clothes/ClothingList.jsx
+++ src/components/Clothes/ClothingList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import ClothingItem from '../ClothingItem/ClothingItem';
+import ClothingItem from './ClothingItem';
 
 function ClothingList() {
   const dispatch = useDispatch();
